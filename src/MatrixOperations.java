c2eb96f80ced245563ccadf1a7598eca3c93359e
SHIVANSHAGL05 <149140668+SHIVANSHAGL05@users.noreply.github.com>
2025-03-06 21:07:07 +0530

    Initial commit

---
+++ src/MatrixOperations.java
@@ -0,0 +1,83 @@
+import java.util.Scanner;
+
+public class MatrixOperations {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the dimensions of the matrices (rows and columns): ");
+        int rows = scanner.nextInt();
+        int cols = scanner.nextInt();
+
+        int[][] matrix1 = new int[rows][cols];
+        int[][] matrix2 = new int[rows][cols];
+
+        System.out.println("Enter elements of Matrix 1:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                matrix1[i][j] = scanner.nextInt();
+            }
+        }
+
+        System.out.println("Enter elements of Matrix 2:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                matrix2[i][j] = scanner.nextInt();
+            }
+        }
+
+        System.out.println("Addition:");
+        printMatrix(addMatrices(matrix1, matrix2));
+
+        System.out.println("Subtraction:");
+        printMatrix(subtractMatrices(matrix1, matrix2));
+
+        System.out.println("Multiplication:");
+        printMatrix(multiplyMatrices(matrix1, matrix2));
+
+        scanner.close();
+    }
+
+    public static int[][] addMatrices(int[][] m1, int[][] m2) {
+        int rows = m1.length, cols = m1[0].length;
+        int[][] result = new int[rows][cols];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                result[i][j] = m1[i][j] + m2[i][j];
+            }
+        }
+        return result;
+    }
+
+    public static int[][] subtractMatrices(int[][] m1, int[][] m2) {
+        int rows = m1.length, cols = m1[0].length;
+        int[][] result = new int[rows][cols];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                result[i][j] = m1[i][j] - m2[i][j];
+            }
+        }
+        return result;
+    }
+
+    public static int[][] multiplyMatrices(int[][] m1, int[][] m2) {
+        int rows = m1.length, cols = m2[0].length, common = m1[0].length;
+        int[][] result = new int[rows][cols];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                for (int k = 0; k < common; k++) {
+                    result[i][j] += m1[i][k] * m2[k][j];
+                }
+            }
+        }
+        return result;
+    }
+
+    public static void printMatrix(int[][] matrix) {
+        for (int[] row : matrix) {
+            for (int val : row) {
+                System.out.print(val + " ");
+            }
+            System.out.println();
+        }
+    }
+}
